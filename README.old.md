b83802a126cdc75cb4a03ace0ecb38450675d2df
Lucjan Kuźniar <kuzniar.lucjan@gmail.com>
2022-02-17 01:30:04 +0100

    initial commit with react template, typescript, tailwind, eslint

---
+++ README.old.md
@@ -0,0 +1,2 @@
+# Filmder
+Single-page application for selecting a movie to watch using your phone.
---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import IndexPage from "./components/pages/IndexPage";
+
+function App() {
+  return (
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<IndexPage/>}/>
+        </Routes>
+      </BrowserRouter>
+
+  );
+}
+
+export default App;

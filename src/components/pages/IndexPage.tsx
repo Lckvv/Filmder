b9d3e386694d9576f5ddbbee8a26119fb282932f
Lucjan Kuźniar <kuzniar.lucjan@gmail.com>
2022-02-17 03:11:02 +0100

    fakedata with fetching

--- src/components/pages/IndexPage.tsx
+++ src/components/pages/IndexPage.tsx
@@ -1,10 +1,38 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import TemplatePage from "../../templates/TemplatePage";
 
 const IndexPage = () => {
+    const [data, setData] = useState(
+        [{
+            id: "1",
+            imageUrl: "",
+            title: "",
+            summary: "",
+            rating: ""
+        }]
+    );
+
+    // @ts-ignore
+    useEffect(() => {
+        const fetchData = async () => {
+            const result = (await require('../../app/data/films.json'));
+
+            setData(result)
+            console.log(result)
+        }
+
+        fetchData();
+    }, []);
+
     return (
         <TemplatePage className={"py-20"}>
-            test
+            <ul>
+                {data !== undefined && data.map(item => (
+                    <li key={item.title}>
+                        {item.title}
+                    </li>
+                ))}
+            </ul>
         </TemplatePage>
     );
 };

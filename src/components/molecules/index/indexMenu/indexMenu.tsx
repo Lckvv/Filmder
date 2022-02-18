774270d6dfe5c5c9901a1b81d182be28e78e97fa
Lucjan Kuźniar <kuzniar.lucjan@gmail.com>
2022-02-18 12:54:32 +0100

    mobile buttons update

--- src/components/molecules/index/indexMenu/indexMenu.tsx
+++ src/components/molecules/index/indexMenu/indexMenu.tsx
@@ -24,23 +24,27 @@ const IndexMenu: FC<Props> = ({data, handleChange, handleDrag, position}) => {
             &&
             data.length > 0
             &&
-                <CardAtom key={data[0].id} imageUrl={data[0].imageUrl} title={data[0].title} summary={data[0].summary}
-                          rating={data[0].rating} wrapClassName={"laptop:h-[730px] tablet:h-[420px]"} position={position} handleDrag={handleDrag}/>
+            <CardAtom key={data[0].id} imageUrl={data[0].imageUrl} title={data[0].title} summary={data[0].summary}
+                      rating={data[0].rating} wrapClassName={"laptop:h-[730px] tablet:h-[420px]"} position={position}
+                      handleDrag={handleDrag}/>
             }
             {data.length > 0 &&
-            <div className={"w-full flex justify-between px-100"}>
-                <button
-                    className={"w-[100px] bg-red-700 hover:bg-red-900 text-white font-bold py-2 px-4 rounded-full"}
-                    onClick={handleChange(data[0].id, data[0].imageUrl, data[0].title, data[0].summary, data[0].rating, false)}>
-                    Reject
-                </button>
+            innerWidth > 900 ?
+                <div className={"w-full flex justify-between px-100"}>
+                    <button
+                        className={"w-[100px] bg-red-700 hover:bg-red-900 text-white font-bold py-2 px-4 rounded-full"}
+                        onClick={handleChange(data[0].id, data[0].imageUrl, data[0].title, data[0].summary, data[0].rating, false)}>
+                        Reject
+                    </button>
 
-                <button
-                    className={"w-[100px] bg-green-700 hover:bg-green-900 text-white font-bold py-2 px-4 rounded-full"}
-                    onClick={handleChange(data[0].id, data[0].imageUrl, data[0].title, data[0].summary, data[0].rating, true)}>
-                    Accept
-                </button>
-            </div>
+                    <button
+                        className={"w-[100px] bg-green-700 hover:bg-green-900 text-white font-bold py-2 px-4 rounded-full"}
+                        onClick={handleChange(data[0].id, data[0].imageUrl, data[0].title, data[0].summary, data[0].rating, true)}>
+                        Accept
+                    </button>
+                </div>
+                :
+                null
             }
         </div>
     );
